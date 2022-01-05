bcf6cfb315bf86e618605b41878a3cddfee9a75e
Sujit Khanal <10sujitkhanal@gmail.com>
2022-01-05 20:40:18 +0545

    Lab 2

---
+++ Lab/Lab 2/Userdefine package/Demo.java
@@ -0,0 +1,8 @@
+import com.gces.sujit.PackageTry;
+public class Demo {
+    public static void main(String[] args) {
+        PackageTry demo = new PackageTry();
+        demo.showMessage();
+    }
+    
+}
---
+++ Lab/Lab 2/Userdefine package/PackageTry.java
@@ -0,0 +1,9 @@
+package com.gces.sujit;
+public class PackageTry {
+    public void showMessage(){
+        System.out.println("Package");
+    }
+    public static void main(String[] args) {
+        System.out.println("uccess");
+    }
+}
